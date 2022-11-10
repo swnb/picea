582e493aeac79c6cadcaa1f0586523255e65188a
async-rustacean <swnblovego@gmail.com>
2022-11-10 18:28:35 +0800

    chore(example): update examples

--- examples/ground_collision.rs
+++ examples/ground_collision.rs
@@ -1,6 +1,10 @@
-use std::time::SystemTime;
+use std::{process, time::SystemTime};
 
-use nannou::prelude::*;
+use nannou::{
+    event::KeyboardInput,
+    prelude::*,
+    winit::event::{self, DeviceEvent},
+};
 use picea::{
     element::{Element, ElementBuilder},
     math::edge::Edge,
@@ -13,7 +17,7 @@ struct Model {
     timer: SystemTime,
 }
 
-fn model(_app: &App) -> Model {
+fn create_model(_app: &App) -> Model {
     let mut scene = Scene::new();
 
     let ground = ElementBuilder::new(
@@ -23,19 +27,8 @@ fn model(_app: &App) -> Model {
 
     scene.push_element(ground);
 
-    let element = ElementBuilder::new(
-        ((200., 200.), 100.),
-        MetaBuilder::new(1.)
-            .angular(std::f32::consts::FRAC_PI_6)
-            .force("gravity", (0., -10.)), // .is_fixed(true),
-    );
-
-    let element: Element = element.into();
-
-    scene.push_element(element);
-
     // let element = ElementBuilder::new(
-    //     (7, (50., 200.), 100.),
+    //     ((200., 200.), 100.),
     //     MetaBuilder::new(1.)
     //         .angular(std::f32::consts::FRAC_PI_6)
     //         .force("gravity", (0., -10.)), // .is_fixed(true),
@@ -45,19 +38,40 @@ fn model(_app: &App) -> Model {
 
     // scene.push_element(element);
 
+    let element = ElementBuilder::new(
+        (7, (50., 200.), 100.),
+        MetaBuilder::new(1.)
+            .angular(std::f32::consts::FRAC_PI_6)
+            .force("gravity", (0., -10.)), // .is_fixed(true),
+    );
+
+    let element: Element = element.into();
+
+    scene.push_element(element);
+
     Model {
         scene,
         timer: SystemTime::now(),
     }
 }
 
-fn event(_app: &App, model: &mut Model, event: Event) {
-    let now = SystemTime::now();
-    let duration = now.duration_since(model.timer).unwrap();
-    model.timer = now;
-    model
-        .scene
-        .update_elements_by_duration(duration.as_secs_f32(), |_| {})
+fn event(app: &App, model: &mut Model, event: Event) {
+    match event {
+        Event::WindowEvent {
+            id: _,
+            simple: Some(WindowEvent::KeyPressed(event::VirtualKeyCode::R)),
+        } => *model = create_model(app),
+        Event::Update(_) => {
+            let now = SystemTime::now();
+
+            let duration = now.duration_since(model.timer).unwrap();
+            model.timer = now;
+            model
+                .scene
+                .update_elements_by_duration(duration.as_secs_f32(), |_| {})
+        }
+        _ => {}
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -107,5 +121,8 @@ fn view(app: &App, model: &Model, frame: Frame) {
 }
 
 fn main() {
-    nannou::app(model).event(event).simple_window(view).run();
+    nannou::app(create_model)
+        .event(event)
+        .simple_window(view)
+        .run();
 }
