826855e78639e0c944c285576140e74eaf1c12fc
async-rustacean <swnblovego@gmail.com>
2023-02-23 18:58:29 +0800

    fix(algo): is_point_inside_shape ; fix error algo

--- src/algo/common.rs
+++ src/algo/common.rs
@@ -7,11 +7,15 @@ pub fn is_point_inside_shape(
     edge_iter: &'_ mut dyn Iterator<Item = Edge<'_>>,
 ) -> bool {
     let mut cross_count: usize = 0;
-    let offset_vector: Vector<f32> = ((0., 0.).into(), point).into();
+    let offset_vector: Vector<f32> = (point, (0., 0.).into()).into();
 
     let is_point_cross_segment = |p1: Point<f32>, p2: Point<f32>| {
+        if (p1.y() * p2.y()).is_sign_positive() {
+            return false;
+        }
+
         let p1y_sub_p2y = p1.y() - p2.y();
-        if p1y_sub_p2y <= f32::EPSILON {
+        if p1y_sub_p2y.abs() <= f32::EPSILON {
             // parallel
             false
         } else {
@@ -26,15 +30,22 @@ pub fn is_point_inside_shape(
                 start_point,
                 end_point,
                 ..
-            } => is_point_cross_segment(*start_point + offset_vector, *end_point + offset_vector),
+            } => unimplemented!(),
             Edge::Circle {
                 center_point,
                 radius,
-            } => center_point.to_vector().abs() <= radius,
+            } => (center_point.to_vector() + offset_vector).abs() <= radius,
             Edge::Line {
                 start_point,
                 end_point,
-            } => is_point_cross_segment(*start_point + offset_vector, *end_point + offset_vector),
+            } => {
+                let result = is_point_cross_segment(
+                    *start_point + offset_vector,
+                    *end_point + offset_vector,
+                );
+
+                result
+            }
         };
         if is_cross {
             cross_count += 1;
@@ -42,4 +53,42 @@ pub fn is_point_inside_shape(
     }
 
     cross_count % 2 != 0
+}
+
+pub fn is_point_inside_shape_debug<'a>(
+    point: Point<f32>,
+    edge_iter: &'a mut dyn Iterator<Item = Edge<'_>>,
+) -> Vec<Edge<'a>> {
+    vec![]
+}
+
+mod test {
+    use crate::math::{point::Point, vector::Vector};
+
+    use super::is_point_inside_shape;
+
+    #[test]
+    fn test_is_point_inside_shape() {
+        let is_point_cross_segment = |p1: Point<f32>, p2: Point<f32>| {
+            if (p1.y() * p2.y()).is_sign_positive() {
+                return false;
+            }
+            let p1y_sub_p2y = p1.y() - p2.y();
+            if p1y_sub_p2y.abs() <= f32::EPSILON {
+                // parallel
+                false
+            } else {
+                let cross_point_x = p1.x() + (p1.y() * (p2.x() - p1.x()) * p1y_sub_p2y.recip());
+                dbg!("{}", cross_point_x);
+                cross_point_x.is_sign_positive()
+            }
+        };
+        let p1: Point<f32> = (-31.55, 142.13).into();
+        let p2: Point<f32> = (-46.091, 227.683).into();
+        let offset_vector: Vector<f32> = (82.6, -175.48).into();
+        assert!(is_point_cross_segment(
+            p1 + offset_vector,
+            p2 + offset_vector
+        ));
+    }
 }
