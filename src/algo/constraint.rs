c240e4fb7661940be6649b12a80b02417bb97e89
async-rustacean <swnblovego@gmail.com>
2023-03-05 16:34:41 +0800

    chore

--- src/algo/constraint.rs
+++ src/algo/constraint.rs
@@ -65,56 +65,51 @@ pub fn compute_constraint<T: Element>(element: &mut T, delta_t: f32) {
 
     let center_point = element.center_point();
 
-    let collision_info = element.meta().collision_infos().next();
-
     let inv_mass = element.meta().inv_mass();
 
-    if let Some((velocity_reducer, angular_velocity_reducer)) =
-        collision_info.map(|collision_info| {
-            use ContactType::*;
-            let contact_point = match collision_info.contact_points() {
-                (Edge(edge_a), Edge(edge_b)) => {
-                    // TODO 物体堆叠
-                    unimplemented!();
-                }
-                (Edge(edge_a), Point(point_b)) => *point_b,
-                (Point(point_a), Edge(edge_b)) => *point_a,
-                (Point(point_a), Point(point_b)) => *point_a,
-            };
+    let Some(collision_info) = element.meta().collision_infos().next() else {
+        return
+    };
 
-            let r: Vector<f32> = (center_point, contact_point).into();
-            let mut normal = collision_info.normal;
+    use ContactType::*;
+    let contact_point = match collision_info.contact_points() {
+        (Edge(edge_a), Edge(edge_b)) => {
+            // TODO 物体堆叠
+            unimplemented!();
+        }
+        (Edge(edge_a), Point(point_b)) => *point_b,
+        (Point(point_a), Edge(edge_b)) => *point_a,
+        (Point(point_a), Point(point_b)) => *point_a,
+    };
 
-            if normal * r > 0. {
-                normal = -normal
-            }
+    let r: Vector<f32> = (center_point, contact_point).into();
+    let mut normal = collision_info.normal;
 
-            let mass_eff = element.meta().compute_mass_eff(normal, r);
-            let inv_moment_of_inertia = element.meta().inv_moment_of_inertia();
-            let depth = collision_info.depth;
-
-            let lambda = mass_eff;
-
-            // let B 0..1 ;let h = t; let b = B/h * depth
-            const B: f32 = 0.9;
-
-            let v = element.compute_point_velocity(contact_point);
-
-            let velocity_reducer = move |pre_velocity: Vector<f32>| {
-                pre_velocity
-                    + normal * ((v * -normal + B * depth * delta_t.recip()) * lambda * inv_mass)
-            };
-
-            let angular_velocity_reducer = move |pre_angular_velocity| {
-                pre_angular_velocity - (r ^ normal) * lambda * inv_moment_of_inertia
-            };
-
-            (velocity_reducer, angular_velocity_reducer)
-        })
-    {
-        element
-            .meta_mut()
-            .set_velocity(velocity_reducer)
-            .set_angular_velocity(angular_velocity_reducer);
+    if normal * r > 0. {
+        normal = -normal
     }
+
+    let mass_eff = element.meta().compute_mass_eff(normal, r);
+    let inv_moment_of_inertia = element.meta().inv_moment_of_inertia();
+    let depth = collision_info.depth;
+
+    let lambda = mass_eff;
+
+    // let B 0..1 ;let h = t; let b = B/h * depth
+    const B: f32 = 0.9;
+
+    let v = element.compute_point_velocity(contact_point);
+
+    let velocity_reducer = move |pre_velocity: Vector<f32>| {
+        pre_velocity + normal * ((v * -normal + B * depth * delta_t.recip()) * lambda * inv_mass)
+    };
+
+    let angular_velocity_reducer = move |pre_angular_velocity| {
+        pre_angular_velocity - (r ^ normal) * lambda * inv_moment_of_inertia
+    };
+
+    element
+        .meta_mut()
+        .set_velocity(velocity_reducer)
+        .set_angular_velocity(angular_velocity_reducer);
 }
