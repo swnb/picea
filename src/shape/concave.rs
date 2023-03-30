36499a7ca4e7a91d2524b79d5cb4cddd0925a9fd
async-rustacean <swnblovego@gmail.com>
2023-03-30 18:12:50 +0800

    feat(concave): add concave constructor method

--- src/shape/concave.rs
+++ src/shape/concave.rs
@@ -1,24 +1,18 @@
 use crate::math::point::Point;
 
-use super::{convex::ConvexPolygon, utils::check_is_concave};
+use super::{convex::ConvexPolygon, utils::split_concave_polygon};
 
 #[derive(Default)]
 pub struct ConcavePolygon {
     sub_convex_polygons: Vec<ConvexPolygon>,
 }
 
-fn split_concave_polygon(vertexes: &[Point]) -> [ConvexPolygon; 2] {
-    todo!()
-}
-
 impl ConcavePolygon {
-    pub fn new(vertexes: impl Into<Vec<Point>>) -> Self {
-        let vertexes: Vec<_> = vertexes.into();
-        let sub_convex_polygons = if check_is_concave(&vertexes) {
-            vec![ConvexPolygon::new(vertexes)]
-        } else {
-            vec![]
-        };
+    pub fn new(vertexes: &[Point]) -> Self {
+        let sub_convex_polygons = split_concave_polygon(vertexes)
+            .into_iter()
+            .map(ConvexPolygon::new)
+            .collect();
         Self {
             sub_convex_polygons,
         }
