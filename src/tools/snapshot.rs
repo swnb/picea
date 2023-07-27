c99b6eba4b233c4b102b2d365845384ac0f8074c
async-rustacean <swnblovego@gmail.com>
2023-07-27 15:04:11 +0800

    feat(snapshot): add more meta property for raw code

--- src/tools/snapshot.rs
+++ src/tools/snapshot.rs
@@ -19,20 +19,50 @@ pub fn create_element_construct_code_snapshot(element: &Element) -> String {
     let is_concave = check_is_concave(&points);
 
     for point in points {
-        tmp_string.push_str(&format!("({},{}).into(),", point.x, point.y));
+        tmp_string.push_str(&format!("({:.3},{:.3}).into(),", point.x, point.y));
     }
+    let raw_vertexes = tmp_string;
 
+    let mass = element.meta().mass();
     let angular_velocity = element.meta().angular_velocity();
     let velocity = element.meta().velocity();
+    let is_fixed = element.meta().is_fixed();
 
-    let element_type = if is_concave { "Concave" } else { "Convex" };
+    let element_type = if is_concave {
+        "ConcavePolygon"
+    } else {
+        "ConvexPolygon"
+    };
+
+    let mut forces = String::new();
+
+    element.meta().force_group().iter().for_each(|(name, f)| {
+        forces.push_str(&format!(
+            r#".force("{}", ({:.3}, {:.3}))"#,
+            name,
+            f.get_vector().x(),
+            f.get_vector().y()
+        ))
+    });
+
+    let is_transparent = element.meta().is_transparent();
 
     format!(
-        "let element = ElementBuilder::new({}::new(vec![{}]),MetaBuilder::new(10.).angular_velocity({}).velocity(({},{})));",
+        r#"let element = ElementBuilder::new(
+            {}::new(vec![{}]),
+            MetaBuilder::new({:.3})
+                .angular_velocity({:.3})
+                .velocity(({:.3},{:.3}))
+                .is_transparent({})
+                .is_fixed({}){});"#,
         element_type,
-        tmp_string,
+        raw_vertexes,
+        mass,
         angular_velocity,
         velocity.x(),
         velocity.y(),
+        is_transparent,
+        is_fixed,
+        forces,
     )
 }
