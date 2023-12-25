3ff07972ceb89ffaefdab09f763943b1847f1c6a
swnb <swnblovego@gmail.com>
2023-12-26 01:04:40 +0800

    chore(collision): mark fixme

--- src/collision/mod.rs
+++ src/collision/mod.rs
@@ -210,9 +210,10 @@ fn sweep_and_prune_collision_detection<T, Z>(
     T: CollisionalCollection,
     Z: FnMut(&T::Collider, &T::Collider),
 {
-    if elements.is_empty() {
-        return;
-    }
+    // FIXME
+    // if elements.is_empty() {
+    // return;
+    // }
 
     elements.sort(|a, b| {
         let (min_a_x, _) = a.projection_on_axis(axis);
