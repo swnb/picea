bf85fcf806039946c4802fd7d97313ad1ab9aa50
swnb <swnblovego@gmail.com>
2024-03-31 23:33:17 +0800

    feat(prelude): export some types

--- picea/src/lib.rs
+++ picea/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod algo;
 pub mod collision;
+pub mod constraints;
 pub mod element;
 pub mod math;
 pub mod meta;
@@ -7,15 +8,18 @@ pub mod scene;
 pub mod shape;
 pub mod tools;
 
-pub mod constraints;
-
 pub mod prelude {
+
     pub use super::element::{
         ComputeMomentOfInertia, ElementBuilder, SelfClone, ShapeTraitUnion, ID,
     };
-    pub use super::math::{point::Point, segment::Segment, vector::Vector, FloatNum};
-    pub use super::meta::{Meta, MetaBuilder};
-    pub use super::shape::{CenterPoint, GeometryTransformer};
+
+    pub use super::collision::Projector;
+    pub use super::math::{edge::Edge, point::Point, segment::Segment, vector::Vector, FloatNum};
+    pub use super::meta::{Mass, Meta, MetaBuilder};
+    pub use super::shape::{CenterPoint, EdgeIterable, GeometryTransformer, NearestPoint};
+
+    pub use super::constraints::{JoinConstraintConfig, JoinConstraintConfigBuilder};
 
     // pub use super::shape::{
     // CenterPoint, Circle, ConcavePolygon, ConstRegularPolygon, ConvexPolygon,
